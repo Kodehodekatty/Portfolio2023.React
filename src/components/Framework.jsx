5d97f4404d43e27722e90d0c830f132d8718c1f7
Kathrine <kathrine.kodehode@gmail.com>
2023-02-03 13:36:25 +0100

    Co-authored-by: FredrikMWold <FredrikMWold@users.noreply.github.com>

--- src/components/Framework.jsx
+++ src/components/Framework.jsx
@@ -1,13 +1,8 @@
 import React, { useState, useRef } from "react";
-import shadowme from "../images/Frame 13.png";
-import shadowbody from "../images/secondshadowbody.png";
-import shadowfooter from "../images/shadowfooter.png";
-import fullshadow from "../images/fullshadow.png";
-import styled from "styled-components";
-import style2 from "../components/FrameworkStyles.module.css";
-import faceandbody from "../images/faceandbody.png";
-import fullkatty from "../images/VSC/fullkatty.png";
 
+import styled from "styled-components";
+import style2 from "../FrameworkStyles.module.css";
+import { ShowImage } from "./ShowImage/ShowImage";
 const face = "<face>";
 const body = "<body>";
 const footer = "<footer>";
@@ -21,80 +16,46 @@ const Button = styled.button`
 
 // main shadow me, starter image;
 
-export function ShowImage(props) {
-  return (
-    <img
-      src={
-        {
-          none: fullshadow,
-          face: shadowme,
-          body: props.inSequence ? faceandbody : shadowbody,
-          footer: props.inSequence ? fullkatty : shadowfooter,
-        }[props.partShowing]
-      }
-      className={style2.shadowmain}
-    />
-  );
-}
-
 // the different looking me images; my face, body and footer
 
 export default function Framework({
-  partShowing,
-  setPartShowing,
-  inSequence,
-  setInSequence,
+  showHeader,
+  showBody,
+  showFooter,
+  setShowHeader,
+  setShowBody,
+  setShowFooter,
 }) {
   return (
-    <>
+    <div>
       {" "}
       <div className={style2.something}>
-        <ShowImage partShowing={partShowing} inSequence={inSequence} />
+        <ShowImage
+          showHeader={showHeader}
+          showFooter={showFooter}
+          showBody={showBody}
+        />
         <div className={style2.btnGroup}>
-          {partShowing != "face" && (
-            <Button className={style2.btn1} onClick={ClickHandler}>
-              {face}
-            </Button>
-          )}
-
-          {!partShowing != "body" && (
-            <Button className={style2.btn2} onClick={ClickHandler2}>
-              {body}
-            </Button>
-          )}
-          {!partShowing != "footer" && (
-            <Button className={style2.btn3} onClick={ClickHandler3}>
-              {footer}
-            </Button>
-          )}
+          <Button
+            className={style2.btn1}
+            onClick={() => setShowHeader((prev) => !prev)}
+          >
+            {!showHeader ? face : ""}
+          </Button>
+          <Button
+            className={style2.btn1}
+            onClick={() => setShowBody((prev) => !prev)}
+          >
+            {!showBody ? body : ""}
+          </Button>
+          <Button
+            className={style2.btn1}
+            onClick={() => setShowFooter((prev) => !prev)}
+          >
+            {!showFooter ? footer : ""}
+          </Button>
         </div>{" "}
       </div>{" "}
-      {partShowing == "body" || (partShowing == "footer" && inSequence)}
-    </>
+    </div>
   );
-
-  // the buttons showing the different images
-
-  function ClickHandler() {
-    setInSequence(false);
-    setPartShowing("face");
-  }
-
-  function ClickHandler2() {
-    setInSequence(false);
-    if (partShowing == "face") {
-      setInSequence(true);
-    }
-    setPartShowing("body");
-  }
-
-  function ClickHandler3() {
-    setInSequence(false);
-
-    if (partShowing == "body" && inSequence) {
-      setInSequence(true);
-    }
-
-    setPartShowing("footer");
-  }
 }
---
+++ src/components/Framework/ShowImage/ShowImage.jsx
@@ -0,0 +1,43 @@
+import shadowbody from "../../../images/secondshadowbody.png";
+import shadowface from "../../../images/faceshadow.png";
+import shadowfooter from "../../../images/shadowfooter.png";
+import fullshadow from "../../../images/fullshadow.png";
+import faceandbody from "../../../images/faceandbody.png";
+import fullkatty from "../../../images/VSC/fullkatty.png";
+import bodyandfooter from "../../../images/bodyandfootershadow.png";
+import faceandfooter from "../../../images/faceandfooter.png";
+
+import styled from "styled-components";
+import style2 from "../../FrameworkStyles.module.css";
+
+export function ShowImage({ showHeader, showBody, showFooter }) {
+  return (
+    <>
+      {/* showing shadow me at the start*/}
+      {!showHeader && !showBody && !showFooter && (
+        <img className={style2.shadowmain} src={fullshadow} />
+      )}
+      {showHeader && !showBody && !showFooter && (
+        <img className={style2.shadowmain} src={shadowface} />
+      )}
+      {showHeader && showBody && !showFooter && (
+        <img className={style2.shadowmain} src={faceandbody} />
+      )}
+      {!showHeader && showBody && !showFooter && (
+        <img className={style2.shadowmain} src={shadowbody} />
+      )}
+      {!showHeader && showBody && showFooter && (
+        <img className={style2.shadowmain} src={bodyandfooter} />
+      )}
+      {showHeader && !showBody && showFooter && (
+        <img className={style2.shadowmain} src={faceandfooter} />
+      )}
+      {!showHeader && !showBody && showFooter && (
+        <img className={style2.shadowmain} src={shadowfooter} />
+      )}
+      {showHeader && showBody && showFooter && (
+        <img className={style2.shadowmain} src={fullkatty} />
+      )}
+    </>
+  );
+}
