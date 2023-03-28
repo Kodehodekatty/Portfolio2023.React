5191dada3dbb83957ea3bc115ed9099bfd12e2c3
Kathrine <kathrine.kodehode@gmail.com>
2023-03-28 09:12:59 +0200

    media query update

---
+++ src/components/background/Background.jsx
@@ -0,0 +1,134 @@
+import React, { useState, useRef, useEffect } from "react";
+
+import styles from "./BackgroundStyles.module.css";
+import AboutMe from "../../Pages/aboutMe/AboutMe";
+import FaceNav from "../../Pages/FaceNav";
+import Navbar from "../socialmediameny/Navbar";
+import Footer from "../../Pages/Footer";
+
+export default function Background({
+  showHeader,
+  showBody,
+  showFooter,
+  onNavigated,
+}) {
+  const [textSelector, setTextSelector] = useState("landing page");
+
+  return (
+    <>
+      {showHeader && !showBody && !showFooter && (
+        <div className={styles.maincodingbackground}>
+          <div className={styles.headerwrapper}>
+            <FaceNav
+              setTextSelector={(text) => {
+                setTextSelector(text);
+                onNavigated(text);
+              }}
+            />
+          </div>
+          <div className={styles.socialmediabar}>
+            <Navbar />
+          </div>
+        </div>
+      )}
+      {showHeader && showBody && !showFooter && (
+        <div className={styles.maincodingbackground}>
+          <div className={styles.headerwrapper}>
+            <FaceNav
+              setTextSelector={(text) => {
+                setTextSelector(text);
+                onNavigated(text);
+              }}
+            />
+          </div>
+          <div className={styles.aboutmewrapper}>
+            <AboutMe textSelector={textSelector} />{" "}
+          </div>
+          <div className={styles.socialmediabar}>
+            <Navbar />
+          </div>
+        </div>
+      )}
+      {!showHeader && showBody && showFooter && (
+        <div className={styles.maincodingbackground}>
+          <div className={styles.aboutmewrapper}>
+            <AboutMe textSelector={textSelector} />
+          </div>
+          <div className={styles.socialmediabar}>
+            <Navbar />{" "}
+          </div>
+          <div className={styles.footerbox}>
+            <Footer />
+          </div>
+        </div>
+      )}
+      {!showHeader && showBody && !showFooter && (
+        <div className={styles.maincodingbackground}>
+          <div className={styles.aboutmewrapper}>
+            <AboutMe textSelector={textSelector} />
+          </div>
+
+          <div className={styles.socialmediabar}>
+            <Navbar />{" "}
+          </div>
+        </div>
+      )}
+      {!showHeader && !showBody && showFooter && (
+        <div className={styles.maincodingbackground}>
+          <div className={styles.footerbox}>
+            <Footer />
+          </div>
+          <div className={styles.socialmediabar}>
+            <Navbar />{" "}
+          </div>
+        </div>
+      )}
+      {showHeader && !showBody && showFooter && (
+        <div className={styles.maincodingbackground}>
+          <div className={styles.headerwrapper}>
+            <FaceNav
+              setTextSelector={(text) => {
+                setTextSelector(text);
+                onNavigated(text);
+              }}
+            />
+          </div>
+          <div className={styles.socialmediabar}>
+            <Navbar />
+          </div>
+          <div className={styles.footerbox}>
+            <Footer />
+          </div>
+        </div>
+      )}
+      {showHeader && showBody && showFooter && (
+        <div className={styles.maincodingbackground}>
+          <div className={styles.headerwrapper}>
+            <FaceNav
+              setTextSelector={(text) => {
+                setTextSelector(text);
+                onNavigated(text);
+              }}
+            />
+          </div>
+          <div className={styles.aboutmewrapper}>
+            <AboutMe textSelector={textSelector} />{" "}
+          </div>
+          <div className={styles.socialmediabar}>
+            <Navbar />
+          </div>
+          <div className={styles.footerbox}>
+            <Footer />
+          </div>
+        </div>
+      )}
+      {!showHeader && !showBody && !showFooter && (
+        <div className={styles.maincodingbackground}>
+          <div className={styles.socialmediabar}>
+            <Navbar />{" "}
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
